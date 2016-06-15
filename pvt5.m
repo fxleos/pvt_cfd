0e4243f23a66717f88780c299777f3e22e22151d
fxleos <fxleos@hotmail.com>
2016-06-15 19:02:19 +0200

    pvt4 for P141 pvt5 for N

--- pvt5.m
+++ pvt5.m
@@ -1,4 +1,3 @@
-
 %% 2-d simulation for pvt
 %Xingliang Fang
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -6,8 +5,8 @@ close all;
 
 %% Define Geometry and Mesh
 % Overall dimensions
-x_d=1.900;   %length in longtitute direction, m
-y_d=0.071*2;   %length in latitute direction
+x_d=0.940;   %length in longtitute direction, m
+y_d=0.071;   %length in latitute direction
 z_d=0.002;   %length in thickness direction
 % Select channel type
 channel_type='N';   %Channel type: 'P', 'U', 'N','P141'
@@ -30,7 +29,7 @@ Pr = 7;
 %% Initialization
 t_pv_ini=25;    %initial temperature for pv
 t_pv=t_pv_ini*ones(x_m,y_m);
-t_water_ini=25; %initial temperature for water
+t_water_ini=15; %initial temperature for water
 t_water=t_water_ini*channel_flag;
 %ambient temperature
 t_a =25;        
@@ -38,22 +37,16 @@ t_a =25;
 I=1000;
 I_m=I*x_m_d*y_m_d;  %elemental I
 %flow velocity
-velo_amp=0.2;
+velo_amp=0.05;
 calculate_velocity;
 
 mass_rate=m*velo_amp*density_water*a_cross;
 Re = density_water*(abs(u)+abs(v))*velo_amp*d_h/viscocity_water;
-%Re = density_water*abs(velo_in)*d_h/viscocity_water;
-%if Re>2300
-    %Nu= 0.023*Re^0.8*Pr^0.4;
-%else
-    %Nu=6;
-%end
-%h_w = Nu*lamda_water/d_h;
 
 
 
-%%simulation
+
+%% simulation
 % iteration steps
 it=1000;
 
@@ -61,11 +54,11 @@ for i = 1:it
     x=1;
     
     y=1;
-        if Re(x,y)>2300
+    if Re(x,y)>2300
         if t_pv>t_water
-            Nu= 0.023*Re^0.8*Pr^0.4;
+            Nu= 0.023*Re(x,y)^0.8*Pr^0.4;
         else
-            Nu= 0.023*Re^0.8*Pr^0.3;
+            Nu= 0.023*Re(x,y)^0.8*Pr^0.3;
         end
     else
         if t_pv>t_water
@@ -80,9 +73,7 @@ for i = 1:it
         channel_flag(x,y)*h_w*t_water(x,y)*x_m_d*p_cross)...
         /(h_a*(y_m_d*z_d+x_m_d*y_m_d)+lamda_pv*z_d*(x_m_d/y_m_d+y_m_d/x_m_d)...
         +channel_flag(x,y)*h_w*x_m_d*p_cross);
-    %t_pv(1,1) = (I_m+h_a*t_a*(y_m_d*z_d+x_m_d*y_m_d)+lamda_pv*z_d*(t_pv(1,2)*...
-    %    x_m_d/y_m_d+t_pv(2,1)*y_m_d/x_m_d))/(h_a*(y_m_d*z_d+x_m_d*y_m_d)...
-    %    +lamda_pv*z_d*(x_m_d/y_m_d+y_m_d/x_m_d));
+
 
         
     for y = 2:y_m-1
@@ -91,11 +82,7 @@ for i = 1:it
             +channel_flag(x,y)*h_w*t_water(x,y)*x_m_d*p_cross)...
             /(h_a*(y_m_d*z_d+x_m_d*y_m_d)+lamda_pv*z_d*...
             (2*x_m_d/y_m_d+y_m_d/x_m_d)+channel_flag(x,y)*h_w*x_m_d*p_cross);
-        %t_pv(x,y) = (I_m+h_a*t_a*(y_m_d*z_d+x_m_d*y_m_d)+lamda_pv*z_d*...
-        %    ((t_pv(x,y+1)+t_pv(x,y-1))*x_m_d/y_m_d+t_pv(x+1,y)*y_m_d/x_m_d)...
-        %    +channel_flag(x,y)*h_w*t_water(x,y)*x_m_d*p_cross)...
-        %    /(h_a*(y_m_d*z_d+x_m_d*y_m_d)+lamda_pv*z_d*...
-        %    (2*x_m_d/y_m_d+y_m_d/x_m_d)+channel_flag(x,y)*h_w*x_m_d*p_cross);
+
         
     if channel_flag(x,y)==1
     switch 3*u(x,y)+v(x,y)
@@ -128,9 +115,7 @@ for i = 1:it
         channel_flag(x,y)*h_w*t_water(x,y)*x_m_d*p_cross)...
         /(h_a*(y_m_d*z_d+x_m_d*y_m_d)+lamda_pv*z_d*(x_m_d/y_m_d+y_m_d/x_m_d)...
         +channel_flag(x,y)*h_w*x_m_d*p_cross);
-    %t_pv(1, y_m) = (I_m+h_a*t_a*(y_m_d*z_d+x_m_d*y_m_d)+lamda_pv*z_d*(t_pv(1,y_m-1)*...
-    %    x_m_d/y_m_d+t_pv(2,y_m)*y_m_d/x_m_d))/(h_a*(y_m_d*z_d+x_m_d*y_m_d)...
-    %    +lamda_pv*z_d*(x_m_d/y_m_d+y_m_d/x_m_d));
+
     if channel_flag(x,y)==1
     switch 3*u(x,y)+v(x,y)
         case 3
@@ -152,9 +137,9 @@ for i = 1:it
     end
     if Re(x,y)>2300
         if t_pv>t_water
-            Nu= 0.023*Re^0.8*Pr^0.4;
+            Nu= 0.023*Re(x,y)^0.8*Pr^0.4;
         else
-            Nu= 0.023*Re^0.8*Pr^0.3;
+            Nu= 0.023*Re(x,y)^0.8*Pr^0.3;
         end
     else
         if t_pv>t_water
@@ -197,9 +182,9 @@ for i = 1:it
         end
         if Re(x,y)>2300
             if t_pv>t_water
-                Nu= 0.023*Re^0.8*Pr^0.4;
+                Nu= 0.023*Re(x,y)^0.8*Pr^0.4;
             else
-                Nu= 0.023*Re^0.8*Pr^0.3;
+                Nu= 0.023*Re(x,y)^0.8*Pr^0.3;
             end
         else
             if t_pv>t_water
@@ -242,9 +227,9 @@ for i = 1:it
                 end
                 if Re(x,y)>2300
                     if t_pv>t_water
-                        Nu= 0.023*Re^0.8*Pr^0.4;
+                        Nu= 0.023*Re(x,y)^0.8*Pr^0.4;
                     else
-                        Nu= 0.023*Re^0.8*Pr^0.3;
+                        Nu= 0.023*Re(x,y)^0.8*Pr^0.3;
                     end
                 else
                     if t_pv>t_water
@@ -267,12 +252,7 @@ for i = 1:it
             channel_flag(x,y)*h_w*t_water(x,y)*x_m_d*p_cross)...
             /(h_a*(2*x_m_d*y_m_d)+lamda_pv*z_d*(x_m_d/y_m_d+2*y_m_d/x_m_d)...
             +channel_flag(x,y)*h_w*x_m_d*p_cross);
-        %t_pv(x,1) = (I_m+h_a*t_a*(x_m_d*y_m_d)+lamda_pv*z_d*((t_pv(x-1,1)...
-        %    +t_pv(x+1,1))*y_m_d/x_m_d+t_pv(x,2)*x_m_d/y_m_d))...
-        %    /(h_a*(x_m_d*y_m_d)+lamda_pv*z_d*(x_m_d/y_m_d+2*y_m_d/x_m_d));
-        %t_pv(x,y_m) = (I_m+h_a*t_a*(x_m_d*y_m_d)+lamda_pv*z_d*((t_pv(x-1,y_m)...
-        %    +t_pv(x+1,y_m))*y_m_d/x_m_d+t_pv(x,y_m-1)*x_m_d/y_m_d))...
-        %    /(h_a*(x_m_d*y_m_d)+lamda_pv*z_d*(x_m_d/y_m_d+2*y_m_d/x_m_d));
+
         if channel_flag(x,y)==1
             switch 3*u(x,y)+v(x,y)
                 case 3
@@ -294,9 +274,9 @@ for i = 1:it
             end
             if Re(x,y)>2300
                 if t_pv>t_water
-                    Nu= 0.023*Re^0.8*Pr^0.4;
+                    Nu= 0.023*Re(x,y)^0.8*Pr^0.4;
                 else
-                    Nu= 0.023*Re^0.8*Pr^0.3;
+                    Nu= 0.023*Re(x,y)^0.8*Pr^0.3;
                 end
             else
                 if t_pv>t_water
@@ -341,9 +321,9 @@ for i = 1:it
     end
     if Re(x,y)>2300
         if t_pv>t_water
-            Nu= 0.023*Re^0.8*Pr^0.4;
+            Nu= 0.023*Re(x,y)^0.8*Pr^0.4;
         else
-            Nu= 0.023*Re^0.8*Pr^0.3;
+            Nu= 0.023*Re(x,y)^0.8*Pr^0.3;
         end
     else
         if t_pv>t_water
@@ -384,9 +364,9 @@ for i = 1:it
             end
             if Re(x,y)>2300
                 if t_pv>t_water
-                    Nu= 0.023*Re^0.8*Pr^0.4;
+                    Nu= 0.023*Re(x,y)^0.8*Pr^0.4;
                 else
-                    Nu= 0.023*Re^0.8*Pr^0.3;
+                    Nu= 0.023*Re(x,y)^0.8*Pr^0.3;
                 end
             else
                 if t_pv>t_water
@@ -428,9 +408,9 @@ for i = 1:it
     end
     if Re(x,y)>2300
         if t_pv>t_water
-            Nu= 0.023*Re^0.8*Pr^0.4;
+            Nu= 0.023*Re(x,y)^0.8*Pr^0.4;
         else
-            Nu= 0.023*Re^0.8*Pr^0.3;
+            Nu= 0.023*Re(x,y)^0.8*Pr^0.3;
         end
     else
         if t_pv>t_water
@@ -454,4 +434,4 @@ subplot(2,1,2)
 clabel(C,h)
 
 %% Electric efficiency
-eff_ele=15+15*0.38*(25-sum(sum(t_pv))/numel(t_pv))/100
+eff_ele=15+0.38*(25-sum(sum(t_pv))/numel(t_pv))
